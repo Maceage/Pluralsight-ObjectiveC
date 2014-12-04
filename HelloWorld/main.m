2057397473fe3d84aa3e97438fa40ab4140f53ef
Graham Mace <graham@maceage.com>
2014-12-04 21:07:12 +0800

    Initial Commit

---
+++ HelloWorld/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Graham Mace on 4/12/2014.
+//  Copyright (c) 2014 Graham Mace. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
